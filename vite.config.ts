caceaee19aa50bf999956a2e643e7d5af8da1051
Daniel Nunes <danyribeironunes15@gmail.com>
2026-03-13 01:16:38 +0000

    chore: Add nitro back

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
@@ -13,6 +14,7 @@ const config = defineConfig({
         }),
         tailwindcss(),
         tanstackStart(),
+        nitro(),
         viteReact(),
     ],
 });
