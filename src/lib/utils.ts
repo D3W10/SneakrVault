4e827daabc41d4f98b639259b03bd214d216b734
Daniel Nunes <danyribeironunes15@gmail.com>
2026-03-15 13:28:17 +0000

    fix: Fix search by owners

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,7 +15,7 @@ export function filterBySearch(sneakers: Sneaker[], search: Search) {
         sneaker.name.toLowerCase().includes(search.term.toLowerCase()) &&
         (!search.location || sneaker.location._id === search.location) &&
         (!search.brand || sneaker.brand._id === search.brand) &&
-        (!search.owner || sneaker.owner === search.owner) &&
+        (!search.owner || sneaker.owner._id === search.owner) &&
         (!search.type || sneaker.type === search.type) &&
         (search.decommissioned === undefined ? !sneaker.decommissioned : search.decommissioned ? sneaker.decommissioned : true)
     );
