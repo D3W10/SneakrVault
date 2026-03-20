9ef32f37669dc2fa3f238786c5649a73765fe828
Daniel Nunes <danyribeironunes15@gmail.com>
2026-03-20 13:41:18 +0000

    feat: Add glow to the sneaker card when it's the pair's birthday

--- src/components/SneakerCard.tsx
+++ src/components/SneakerCard.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@tanstack/react-router";
-import { IconCalendarEvent, IconMapPin } from "@tabler/icons-react";
+import { IconCake, IconCalendarEvent, IconMapPin } from "@tabler/icons-react";
 import { format, isSameDay, parseISO } from "date-fns";
 import { Skeleton } from "@/components/ui/skeleton"
 import { SneakerPhoto } from "@/components/SneakerPhoto";
@@ -17,28 +17,31 @@ export function SneakerCard({ sneaker, birthday }: SneakerCardProps) {
     const birthdayLabel = birthdayDate ? (isBirthdayToday ? "Today" : format(birthdayDate, "d MMM")) : "";
 
     return (
-        <Link to="/sneakers/$id" params={{ id: sneaker._id }} className={cn("min-w-60 block relative p-2 pr-6 bg-secondary rounded-2xl hover:shadow-2xl hover:shadow-primary/5 group ring ring-border/75 hover:border-white/20 overflow-hidden transition-shadow duration-300 z-0", !birthday ? "w-full" : "shrink-0")}>
+        <Link to="/sneakers/$id" params={{ id: sneaker._id }} className={cn("min-w-60 block relative p-2 bg-secondary rounded-2xl hover:shadow-2xl hover:shadow-primary/5 group ring ring-border/75 hover:border-white/20 overflow-hidden transition-shadow duration-300 z-0", !birthday ? "w-full" : "pr-8 shrink-0")}>
             <div className="absolute inset-0 bg-linear-to-br from-primary/5 to-primary/2 opacity-0 group-hover:opacity-100 transition-opacity duration-500" />
-            <div className="h-full flex items-center gap-4 relative z-1">
+            <div className="w-fit h-full flex items-center gap-4 relative z-1">
                 <SneakerPhoto sneaker={sneaker} />
                 <div className="min-w-0 flex flex-col justify-center gap-y-2">
                     <div>
                         <h3 className="text-lg text-white group-hover:text-primary-200 font-bold tracking-tight truncate transition-colors">{sneaker.name}</h3>
                         <h3 className="text-sm text-zinc-300 group-hover:text-primary-100 font-medium truncate transition-colors">{sneaker.color}</h3>
                     </div>
-                    <div className="flex items-center gap-1.5 text-zinc-400 group-hover:text-zinc-300 transition-colors">
+                    <div className="flex items-center gap-1.5 text-zinc-400 transition-colors">
                         {!birthday ? (
                             <>
                                 <IconMapPin className="size-4 shrink-0 text-primary" />
-                                <span className="text-primary-100 group-hover:text-primary-300 text-sm font-semibold opacity-75 group-hover:opacity-100 truncate transition">
+                                <span className="text-sm font-semibold opacity-75 group-hover:opacity-100 truncate transition">
                                     {sneaker.location.name}
                                 </span>
                             </>
-                            
                         ) : (
                             <>
-                                <IconCalendarEvent className="size-4 shrink-0 text-primary" />
-                                <span className="text-primary-100 group-hover:text-primary-300 text-sm font-semibold opacity-75 group-hover:opacity-100 truncate transition">
+                                {!isBirthdayToday ?
+                                    <IconCalendarEvent className="size-4 shrink-0 text-primary" />
+                                :
+                                    <IconCake className="size-4 shrink-0 text-primary" />
+                                }
+                                <span className={cn("text-transparent text-sm font-semibold bg-linear-to-r from-zinc-400 via-zinc-400 to-zinc-400 bg-clip-text bg-size-[200%_100%] opacity-75 group-hover:opacity-100 truncate transition duration-300", isBirthdayToday && "via-white animate-text-glow-sweep")}>
                                     {birthdayLabel}
                                 </span>
                             </>
